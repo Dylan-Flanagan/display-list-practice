50b4b7e1829a0ef206826ba4562af79b2a68969f
Dylan <dylflanagan@gmail.com>
2022-09-15 13:17:16 -0700

    Added first list of arrays

---
+++ candies-data.js
@@ -0,0 +1,18 @@
+export const candies = [
+    {
+        name: '100 Grand',
+        flavor: 'chocolate caramel',
+        type: 'old people candy',
+    },
+    {
+        name: 'Bubblicious',
+        flavor: 'strawberry watermelon',
+        type: 'gum',
+    },
+
+    {
+        name: 'Smarties',
+        flavor: 'tart fruity',
+        type: 'waffers',
+    },
+];
